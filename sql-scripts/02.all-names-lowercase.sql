732a47c0153741569ceff2a6080045e6e24e6198
marcelov2 <69441794+marcelov2@users.noreply.github.com>
2021-01-16 20:22:32 -0300

    Update 02.all-names-lowercase.sql

--- sql-scripts/02.all-names-lowercase.sql
+++ sql-scripts/02.all-names-lowercase.sql
@@ -1,19 +1,19 @@
-UPDATE phasmo.ghost_type SET name = 'Espírito' WHERE name = 'Espírito';
+UPDATE phasmo.ghost_type SET name = 'spirit' WHERE name = 'Spirit';
 UPDATE phasmo.ghost_type SET name = 'wraith' WHERE name = 'Wraith';
-UPDATE phasmo.ghost_type SET name = 'Fantasma' WHERE name = 'Fantasma';
+UPDATE phasmo.ghost_type SET name = 'Phantom' WHERE name = 'Phantom';
 UPDATE phasmo.ghost_type SET name = 'poltergeist' WHERE name = 'Poltergeist';
 UPDATE phasmo.ghost_type SET name = 'banshee' WHERE name = 'Banshee';
 UPDATE phasmo.ghost_type SET name = 'jinn' WHERE name = 'Jinn';
 UPDATE phasmo.ghost_type SET name = 'mare' WHERE name = 'Mare';
 UPDATE phasmo.ghost_type SET name = 'revenant' WHERE name = 'Revenant';
-UPDATE phasmo.ghost_type SET name = 'Assombração' WHERE name = 'Assombração';
-UPDATE phasmo.ghost_type SET name = 'Demonio' WHERE name = 'Demonio';
+UPDATE phasmo.ghost_type SET name = 'shade' WHERE name = 'Shade';
+UPDATE phasmo.ghost_type SET name = 'demon' WHERE name = 'Demon';
 UPDATE phasmo.ghost_type SET name = 'yurei' WHERE name = 'Yurei';
 UPDATE phasmo.ghost_type SET name = 'oni' WHERE name = 'Oni';
 
-UPDATE phasmo.evidence SET name = 'Orbe Fantasma' WHERE name = 'Orbe Fantasma';
+UPDATE phasmo.evidence SET name = 'orbe Fantasma' WHERE name = 'Orbe Fantasma';
 UPDATE phasmo.evidence SET name = 'spirit box' WHERE name = 'Spirit Box';
-UPDATE phasmo.evidence SET name = 'Impressões digital' WHERE name = 'Impressões digital';
+UPDATE phasmo.evidence SET name = 'impressões digital' WHERE name = 'Impressões digital';
 UPDATE phasmo.evidence SET name = 'emf level 5' WHERE name = 'EMF Level 5';
-UPDATE phasmo.evidence SET name = 'Temperatura Baixa' WHERE name = 'Temperatura Baixa';
-UPDATE phasmo.evidence SET name = 'Escrita fantasma' WHERE name = 'Escrita fantasma';
+UPDATE phasmo.evidence SET name = 'temperatura Baixa' WHERE name = 'Temperatura Baixa';
+UPDATE phasmo.evidence SET name = 'escrita fantasma' WHERE name = 'Escrita fantasma';
