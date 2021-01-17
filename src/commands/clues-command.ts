5417ea3a01d84d5f99adea99603259d80918a330
marcelov2 <69441794+marcelov2@users.noreply.github.com>
2021-01-16 21:44:02 -0300

    Update clues-command.ts

--- src/commands/clues-command.ts
+++ src/commands/clues-command.ts
@@ -76,7 +76,8 @@ export class CluesCommand extends BotCommand {
 
   private renderSimpleListEmbed(ghosts: GhostType[], evidences: string[][]): MessageEmbed {
     let message = new MessageEmbed().setColor('#666666')
-      .setTitle('Aqui você tem uma lista do que poderia ser :Dancing:')
+      .setTitle('Aqui você tem uma lista do que poderia ser')
+    message.channel.send("https://4.bp.blogspot.com/-DvM9k9cWfzQ/Vze5o0-QRQI/AAAAAAAAArw/BJSiYEzWWOkX6B50Q25L1ZHvWn2X7VIigCLcB/s640/gifs-assustadores-1.gif");
       .addFields([
         { name: "Fantasma", value:'\u200b', inline: true},
         { name: "Evidência ausente 0", value:'\u200b', inline: true},
