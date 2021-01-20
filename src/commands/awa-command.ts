acdac1f12bb09c3f8f043b46330c39b8381b20ad
marcelov2 <69441794+marcelov2@users.noreply.github.com>
2021-01-20 17:27:47 -0300

    Update awa-command.ts

--- src/commands/awa-command.ts
+++ src/commands/awa-command.ts
@@ -1,20 +1,31 @@
 import { Message } from "discord.js";
-
 import { BotCommand } from "../abstracts/bot-command";
 
 export class AwaCommand extends BotCommand {
 
   constructor()   {
-    super("awa", [])
+    super("mapa",[])
   }
 
   shouldExec(message: Message): boolean {
-    let matches = message.content.match(/^.*a+[ ]*w+[ ]*a+.*$/i);
-    return  matches !== null
+    return message.content.startsWith(this.name);
   }
 
-  exec(message: Message) : void {
-    message.channel.send("https://i.gifer.com/kgY.gif");
-  }
+  async exec(message: Message): Promise<void> {
+    let args = message.content.split(" ").map((argument) => argument.toLowerCase());
+    args.shift();
 
-};
+    const rando_imgs = [
+    'https://gph.is/g/E3LpyGO',
+    'https://gph.is/g/aeoWg28',
+    'https://gph.is/g/EvWp5gg',
+    'https://gph.is/g/Z5GJXjD',
+    'https://gph.is/g/ZPGqdrd',
+    'https://gph.is/g/EqW0g2x',
+    'https://gph.is/g/aXVLlgP',
+	];
+
+
+    message.channel.send(rando_imgs[Math.floor(Math.random() * rando_imgs.length)]);
+  }
+}
